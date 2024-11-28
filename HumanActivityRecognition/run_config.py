fc6f5d1a0cbfd1407cd3a8f34b3a7ab7ea04000e
Carolina Di Maio <carolina.dimaio@outlook.it>
2024-11-28 17:25:44 +0100

    update

--- HumanActivityRecognition/run_config.py
+++ HumanActivityRecognition/run_config.py
@@ -1,6 +1,6 @@
 # run_config.py
 
-# Variabili globali +
+# Variabili globali 
 NB_SENSOR_CHANNELS = 9  # Numero di canali (features)
 SLIDING_WINDOW_LENGTH = 100  # Lunghezza della finestra scorrevole
 SLIDING_WINDOW_STEP = 50  # Passo della finestra scorrevole
