9202d2d8b80a5cdd07c2ccbad3ab6c86e2272fc5
Carolina <carolina.dimaio@outlook.it>
2025-02-10 21:45:07 +0100

    update

--- HumanActivityRecognition/train.py
+++ HumanActivityRecognition/train.py
@@ -8,59 +8,59 @@ from utils import plot
 
 train_on_gpu=check_gpu.check_gpu_availability()
 
-#(net=modello da allenare)
-def train(net, train_loader, test_loader,epochs=10,batch_size=16,lr=0.01, patience=5):
+class EarlyStopper:
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = float('inf')
+ 
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
 
-    #ottimizzatore stocastico del gradiente per aggiornare i pesi del modello, momentum aiuta a accelrare la discesa
-    #del gradinete nella dimensione desiderata e ridurre le oscillazioni
-    #wight_decay: regolarizzazione L2 per prevenire overfitting
-    
-    
+def train(net, train_loader, test_loader, epochs=10, batch_size=16, lr=0.01, patience=5):
+
     opt = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
-    #opt = torch.optim.RMSprop(net.parameters(), lr=lr, alpha=0.99, weight_decay=1e-4, momentum=0.9)
-    #opt = torch.optim.Adam(net.parameters(), lr=lr, betas=(0.9, 0.999), eps=1e-8, weight_decay=1e-4)
     criterion = nn.CrossEntropyLoss()
 
     if train_on_gpu:
         net.cuda()
 
-
     train_loss_history = [] 
     val_loss_history = [] 
     val_accuracy_history = []
+    val_f1score_history = []
 
-    best_val_loss = float('inf')
+    best_f1score = 0
     patience_counter = 0
 
-    #per ogni epoca il modello viene allenato su tutti i dati di train
     for e in range(epochs):
         
         train_losses = []
-        net.train()  # Imposta la rete in modalità allenamento
+        net.train()
         
-        #Ciclo sui minibatch del DataLoader di training
         for inputs, targets in train_loader: 
-            #print("Input batch size:", inputs.size(0))
             if train_on_gpu:
                 inputs, targets = inputs.cuda(), targets.cuda()
             
             h = net.init_hidden(batch_size)
-            # h = tuple([each.data for each in h])            
-
-            # zero accumulated gradients
             opt.zero_grad()
-
-            # get the output from the model
             output, h = net(inputs, h, batch_size)
             loss = criterion(output, targets.long())
             train_losses.append(loss.item())
             
-            #backward pass e ottimizzazione
             loss.backward()
             opt.step()
 
         train_loss_history.append(np.mean(train_losses))
-        # Valutazione sui dati di validazione
+
         val_h = net.init_hidden(batch_size)
         val_losses = []
         accuracy = 0
@@ -74,8 +74,7 @@ def train(net, train_loader, test_loader,epochs=10,batch_size=16,lr=0.01, patien
                 if train_on_gpu:
                     inputs, targets = inputs.cuda(), targets.cuda()
                 
-                #Forward pass
-                output, val_h = net(inputs, val_h,batch_size)
+                output, val_h = net(inputs, val_h, batch_size)
                 val_loss = criterion(output, targets.long())
                 val_losses.append(val_loss.item())
 
@@ -84,23 +83,18 @@ def train(net, train_loader, test_loader,epochs=10,batch_size=16,lr=0.01, patien
                 accuracy += torch.mean(equals.type(torch.FloatTensor))
                 f1score += metrics.f1_score(top_class.cpu(), targets.view(*top_class.shape).long().cpu(), average='weighted')
         
-
         val_loss_history.append(np.mean(val_losses)) 
         val_accuracy_history.append(accuracy / len(test_loader))
-        
+        val_f1score_history.append(f1score / len(test_loader))
 
-        net.train()
         print(f"Epoch: {e+1}/{epochs}... "
               f"Train Loss: {np.mean(train_losses):.4f}... "
               f"Val Loss: {np.mean(val_losses):.4f}... "
               f"Val Acc: {accuracy / len(test_loader):.4f}... "
               f"F1-Score: {f1score / len(test_loader):.4f}")
-        
 
-
-        # Early stopping
-        if np.mean(val_losses) < best_val_loss:
-            best_val_loss = np.mean(val_losses)
+        if f1score / len(test_loader) > best_f1score:
+            best_f1score = f1score / len(test_loader)
             patience_counter = 0
         else:
             patience_counter += 1
@@ -108,6 +102,6 @@ def train(net, train_loader, test_loader,epochs=10,batch_size=16,lr=0.01, patien
         if patience_counter >= patience:
             print("Early stopping triggered")
             break
-    plot.plot_learning_curves(train_loss_history, val_loss_history, val_accuracy_history)
 
-    return np.mean(val_losses)
+
+    return best_f1score
