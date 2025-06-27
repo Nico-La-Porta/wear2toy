e45b7f0a914884016b8bdf176167c6e5723e95e2
Nicolò La Porta <nicolo.laporta@supsi.ch>
2025-06-27 11:26:56 +0200

    Update .gitignore and enhance app config structure

--- HumanActivityRecognition/app_config/config.py
+++ HumanActivityRecognition/app_config/config.py
@@ -1,5 +1,6 @@
 import os
 import dotenv
+from pathlib import Path
 
 # Set a default environment if none is provided
 APP_ENV = os.getenv('APP_ENV')
@@ -12,3 +13,24 @@ elif APP_ENV == 'dev':
     from .config_dev import *
 else:
     dotenv.load_dotenv()
+
+    # Configuration - Load from environment variables
+    NEPTUNE_PROJECT = os.getenv("NEPTUNE_PROJECT")
+    NEPTUNE_API_TOKEN = os.getenv("NEPTUNE_API_TOKEN")
+
+    # Paths
+    PROJ_ROOT = Path(__file__).resolve().parents[1]
+
+    DATA_DIR = PROJ_ROOT / "data"
+    RAW_DATA_DIR = DATA_DIR / "raw" #in formato .csv
+    RAW_DATA_DIR_TRAIN = RAW_DATA_DIR/"train" #in formato .csv
+    RAW_DATA_DIR_TEST=RAW_DATA_DIR/"test"  #in formato .csv
+    FEATURES_DATA_DIR = DATA_DIR / "features" #le due matrici di input con feature extracted di ML
+    PROCESSED_DATA_DIR = DATA_DIR / "processed" #in formato .npz
+    PROCESSED_DATA_DIR_TRAIN= PROCESSED_DATA_DIR/"train" #in formato .npz
+    PROCESSED_DATA_DIR_TEST= PROCESSED_DATA_DIR/"test" #in formato .npz
+
+    MODELS_DIR = PROJ_ROOT / "models" #cartella per i modelli salvati
+
+    REPORTS_DIR = PROJ_ROOT / "reports" # Report generati
+    FIGURES_DIR = REPORTS_DIR / "figures" # Grafici inclusi nei report
